5123d8d9337962e09a58b0e227098f9d86f7c0f7
Patricia Parra <patriciaelenaparrabullones@gmail.com>
2026-03-12 16:51:55 +0100

    Refactor functions to use lowercase naming convention

--- SumaconFunciom.py
+++ SumaconFunciom.py
@@ -1,26 +1,26 @@
-def Suma(x,y,z):
-    Resultado=x+y+z
-    return Resultado
+def suma(x,y,z):
+    resultado=x+y+z
+    return resultado
 
-def Multiplicar(X,Y):  
-    Mult=X*Y
-    return Mult
+def multiplicar(x,y):  
+    mult=x*y
+    return mult
     
-R1=Suma(7,5,9)
+R1=suma(7,5,9)
 print("Primera Suma con funcion")
 print(R1)
-R2=Suma(8,5,9)
+R2=suma(8,5,9)
 print("Segunda Suma con funcion")
 print(R2)
-R3=Suma(7,4,9)
+R3=suma(7,4,9)
 print("Tercera Suma con funcion")
 print(R3)
-M1=Multiplicar(2,4)
+M1=multiplicar(2,4)
 print("Primera Multiplicacion")
 print(M1)
-M2=Multiplicar(5,8)
+M2=multiplicar(5,8)
 print("Segunda Multiplicacion")
 print(M2)
-SumaMul=M1+M2
+sumaMul=M1+M2
 print("Suma de ambas multiplicaciones anteriores")
-print(SumaMul)
+print(sumaMul)
